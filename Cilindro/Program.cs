769682ab496fe96e7e2a234c7cf3ccfc48a62468
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-08 12:29:28 +0000

    Add program to calculate volume and surface area of a cilinder (Exercise 6)

---
+++ Cilindro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar variáveis
+            string hStr, radStr;
+            float h, rad, vol, surfArea;
+            float pi = 3.1415926f;
+
+            //Pedir altura do cilindro ao utilizador e converter p/ float
+            Console.WriteLine("Input the cilinder's height:");
+            hStr = Console.ReadLine();
+            h = Convert.ToSingle(hStr);
+            //Pedir o raio do cilindro ao utilizador e converter p/ float
+            Console.WriteLine("Input the radius of the cilinder");
+            radStr = Console.ReadLine();
+            rad = Convert.ToSingle(radStr);
+            //calcular volume e área da superfície
+            vol = pi * (rad*rad) * h;
+            surfArea = 2 * pi * rad * (rad + h);
+
+            /*Mostrar os resultados ao utilizador(usa duas maneiras diferentes
+            para praticar as mesmas, por nenhuma rãzão especial*/
+            Console.WriteLine($"Cilinder's Volume = {vol:f3}");
+            Console.WriteLine("Cilinder's Surface Area = {0:f3}", 
+                surfArea);
+            Console.ReadKey();
+        }
+    }
+}
